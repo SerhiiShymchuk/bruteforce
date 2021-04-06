eab896f2138cbe9bbceea31aab3f783b0e8ddb3e
SerhiiShymchuk <shymaser@gmail.com>
2021-04-06 11:05:49 +0200

    optimize makeIncrementArr

--- bruteforce.js
+++ bruteforce.js
@@ -1,32 +1,33 @@
-
-
-function makeTryGenerator(chars, length) {
-    const indices = Array(length).fill(0)
-
-    return function () {
-        const str = indices.map(i => chars[i]).join('')
-        incrementArr(indices, chars.length-1)
+function makeTryGenerator(chars, length) { //chars='12345' length=3
+    const indices = Array(length).fill(0) //indices=[0,1,1]
+    const incrementArr = makeIncrementArr(indices, chars.length-1)
+    return function () { //trygen fn
+        const str = indices.map(i => chars[i]).join('') //str='121'
+        incrementArr()
         return str
     }
 }
-function incrementArr(nums, max) {
-    for (let i = nums.length-1; i >= 0; i--) {
-        if (nums[i] < max) {
-            nums[i]++
-            break
+function makeIncrementArr(nums, max) { //nums=[0,1,1] max=4
+    const lastIndex = nums.length-1
+    return function () {
+        for (let i = lastIndex; i >= 0; i--) { //i=2
+            if (nums[i] < max) { 0 < 4
+                nums[i]++ 
+                break
+            }
+            nums[i] = 0
         }
-        nums[i] = 0
     }
-    return nums
 }
-function generateTries(chars, length, count) {
-    const trygen = makeTryGenerator(chars, length)
-    const tries = []
-    for (let i = 0; i < count; i++) {
+function generateTries(chars, length, count) { //chars='12345' length=3 count=50
+    const trygen = makeTryGenerator(chars, length) //trygen=line4 fn
+    const tries = [] //tries=['111', '112', '113', '114', '115', '121']
+    for (let i = 0; i < count; i++) { //i=5
         tries.push(trygen())
     }
     return tries
 }
 
-console.log(generateTries('abcdef', 5, 100000))
+console.log(generateTries('12345', 3, 50))
 setTimeout(console.log, 1e8)
+
